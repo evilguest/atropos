8100922bc2a2937925169137362ca1de4c81f92b
mephisto <mephisto@skbkontur.ru>
2020-11-22 19:37:46 +0700

    dictionary: implementation with AVL-tree

--- Atropos/ImmutableDictionary.cs
+++ Atropos/ImmutableDictionary.cs
@@ -1,55 +1,102 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Linq;
 
 namespace Atropos
 {
-    /// <summary>
-    /// Provides the helper methods for the <see cref="ImmutableDictionary{TKey, TValue}"/>
-    /// </summary>
-    public static class ImmutableDictionary
-    {
-        /// <summary>
-        /// Initializes the immutable dictionary based on the provided key-value pairs 
-        /// </summary>
-        /// <typeparam name="TKey">The dictionary key type</typeparam>
-        /// <typeparam name="TValue">The dictionary value type</typeparam>
-        /// <param name="keyValuePairs">The pairs to use for the immutable dictionary</param>
-        /// <returns>A new immutable dictionary containing the provided <paramref name="keyValuePairs"/></returns>
-        public static ImmutableDictionary<TKey, TValue> Init<TKey, TValue>(IEnumerable<(TKey, TValue)> keyValuePairs)
-            => throw new NotImplementedException();
-    }
     /// <summary>
     /// Represents an immutable dictionary
     /// </summary>
     /// <typeparam name="TKey">Type of the key</typeparam>
     /// <typeparam name="TValue">Type of the value</typeparam>
-    public class ImmutableDictionary<TKey, TValue>: IReadOnlyDictionary<TKey, TValue>
+    public class ImmutableDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+        where TKey : IComparable<TKey>
     {
+        private readonly TreeNode<ComparableKeyValuePair> _rootNode;
+
+        public ImmutableDictionary()
+        {
+        }
+
+        private ImmutableDictionary(TreeNode<ComparableKeyValuePair> rootNode, int count)
+        {
+            _rootNode = rootNode;
+            Count = count;
+        }
+
         /// <summary>
         /// Returns the value by the given key
         /// </summary>
         /// <param name="key">The key</param>
         /// <returns></returns>
         /// <exception cref="KeyNotFoundException">Thrown when the given key is not present in the dictionary</exception>
-        public TValue this[TKey key] => throw new NotImplementedException();
+        public TValue this[TKey key]
+        {
+            [Pure]
+            get
+            {
+                if (key == null)
+                {
+                    throw new ArgumentNullException(nameof(key));
+                }
+
+                if (!TryGetValue(key, out var value))
+                {
+                    throw new KeyNotFoundException(key.ToString());
+                }
+
+                return value;
+            }
+        }
 
         /// <summary>
         /// Returns an enumeration of all the keys in this dictionary
         /// </summary>
-        public IEnumerable<TKey> Keys => throw new NotImplementedException();
-
-        //public IEnumerable<TValue> Values => throw new NotImplementedException();
-
-        public int Count => throw new NotImplementedException();
-
-        public IEnumerable<TValue> Values => throw new NotImplementedException();
-
-        public ImmutableDictionary<TKey, TValue> Add(TKey key, TValue value)
+        public IEnumerable<TKey> Keys
         {
-            throw new NotImplementedException();
+            [Pure]
+            get
+            {
+                if (_rootNode == null)
+                {
+                    return new TKey[0];
+                }
+
+                return _rootNode.Select(pair => pair.Key);
+            }
+        }
+
+        public int Count { get; }
+
+        public IEnumerable<TValue> Values
+        {
+            [Pure]
+            get
+            {
+                if (_rootNode == null)
+                {
+                    return new TValue[0];
+                }
+
+                return _rootNode.Select(pair => pair.Value);
+            }
+        }
+
+        [Pure]
+        public ImmutableDictionary<TKey, TValue> Add([NotNull] TKey key, TValue value)
+        {
+            var pair = new ComparableKeyValuePair(key, value);
+            var nodeWithRemovedOldValue = _rootNode?.Remove(pair, out _);
+            if (nodeWithRemovedOldValue == null)
+            {
+                return new ImmutableDictionary<TKey, TValue>(
+                    new TreeNode<ComparableKeyValuePair>(pair), 1);
+            }
+
+            return new ImmutableDictionary<TKey, TValue>(_rootNode.Insert(pair), Count + 1);
         }
 
         public ImmutableDictionary<TKey, TValue> AddRange(IEnumerable<KeyValuePair<TKey, TValue>> pairs)
@@ -67,20 +114,49 @@ namespace Atropos
             throw new NotImplementedException();
         }
 
-        public bool ContainsKey(TKey key)
+        [Pure]
+        public bool ContainsKey([NotNull] TKey key)
         {
-            throw new NotImplementedException();
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return _rootNode?.FindNode(new ComparableKeyValuePair(key, default(TValue))) != null;
         }
 
+        [Pure]
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            if (_rootNode == null)
+            {
+                yield break;
+            }
+
+            foreach (var pair in _rootNode)
+            {
+                yield return new KeyValuePair<TKey, TValue>(pair.Key, pair.Value);
+            }
         }
 
-        public ImmutableDictionary<TKey, TValue> Remove(TKey key)
+        [Pure]
+        public ImmutableDictionary<TKey, TValue> Remove([NotNull] TKey key)
         {
-            throw new NotImplementedException();
+            if (_rootNode == null)
+            {
+                return this;
+            }
+
+            var pair = new ComparableKeyValuePair(key, default(TValue));
+            var nodeWithoutKey = _rootNode.Remove(pair, out var success);
+            if (!success)
+            {
+                return this;
+            }
+
+            return new ImmutableDictionary<TKey, TValue>(nodeWithoutKey, Count - 1);
         }
+
 
         public ImmutableDictionary<TKey, TValue> RemoveRange(IEnumerable<TKey> keys)
         {
@@ -102,14 +178,42 @@ namespace Atropos
             throw new NotImplementedException();
         }
 
-        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        [Pure]
+        public bool TryGetValue(TKey key, out TValue value)
         {
-            throw new NotImplementedException();
+            value = default(TValue);
+            var node = _rootNode?.FindNode(new ComparableKeyValuePair(key, default(TValue)));
+            if (node == null)
+            {
+                return false;
+            }
+
+            value = node.Value.Value;
+            return true;
         }
 
+        [Pure]
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
+        }
+
+        private struct ComparableKeyValuePair : IComparable<ComparableKeyValuePair>
+        {
+            public ComparableKeyValuePair([NotNull] TKey key, TValue value)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public TKey Key { get; }
+
+            public TValue Value { get; }
+
+            public int CompareTo(ComparableKeyValuePair other)
+            {
+                return Key.CompareTo(other.Key);
+            }
         }
     }
 }
---
+++ Atropos/TreeNode.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace Atropos
+{
+    internal class TreeNode<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        public readonly TreeNode<T> Left;
+
+        public readonly TreeNode<T> Right;
+
+        public readonly T Value;
+
+        public readonly int Height;
+
+        public TreeNode([NotNull] T value)
+        {
+            Value = value;
+            Height = 1;
+        }
+
+        private TreeNode([NotNull] TreeNode<T> original, [NotNull] T value)
+        {
+            Left = original.Left;
+            Right = original.Right;
+            Height = original.Height;
+            Value = value;
+        }
+
+        private TreeNode(TreeNode<T> left, TreeNode<T> right, [NotNull] T value)
+        {
+            Left = left;
+            Right = right;
+            Height = Math.Max(left.SafeHeight(), right.SafeHeight()) + 1;
+            Value = value;
+        }
+
+        private TreeNode(
+            TreeNode<T> leftLeft,
+            TreeNode<T> leftRight,
+            TreeNode<T> rightLeft,
+            TreeNode<T> rightRight,
+            [NotNull] T leftValue,
+            [NotNull] T rightValue,
+            [NotNull] T myValue)
+        {
+            Left = new TreeNode<T>(leftLeft, leftRight, leftValue);
+            Right = new TreeNode<T>(rightLeft, rightRight, rightValue);
+            Height = Math.Max(Left.Height, Right.Height) + 1;
+            Value = myValue;
+        }
+
+        [Pure]
+        private TreeNode<T> RotateRight()
+        {
+            Debug.Assert(Left != null);
+
+            var a = Left.Left;
+            var b = Left.Right;
+            var c = Right;
+            var rotatedThis = new TreeNode<T>(b, c, Value);
+            var rotatedLeft = new TreeNode<T>(a, rotatedThis, Left.Value);
+
+            return rotatedLeft;
+        }
+
+        [Pure]
+        private TreeNode<T> RotateLeft()
+        {
+            Debug.Assert(Right != null);
+
+            var a = Left;
+            var b = Right.Left;
+            var c = Right.Right;
+
+            var rotatedThis = new TreeNode<T>(a, b, Value);
+            var rotatedRight = new TreeNode<T>(rotatedThis, c, Right.Value);
+
+            return rotatedRight;
+        }
+
+        [Pure]
+        private TreeNode<T> Balance()
+        {
+            if (this.BalanceFactor() == 2)
+            {
+                Debug.Assert(Right != null);
+
+                if (Right.BalanceFactor() >= 0)
+                {
+                    return RotateLeft();
+                }
+
+                Debug.Assert(Right.Left != null);
+
+                var a = Left;
+                var b = Right.Left.Left;
+                var c = Right.Left.Right;
+                var d = Right.Right;
+
+                return new TreeNode<T>(a, b, c, d, Value, Right.Value, Right.Left.Value);
+            }
+
+            if (this.BalanceFactor() == -2)
+            {
+                Debug.Assert(Left != null);
+
+                if (Left.BalanceFactor() <= 0)
+                {
+                    return RotateRight();
+                }
+
+                Debug.Assert(Left.Right != null);
+
+                var a = Left.Left;
+                var b = Left.Right.Left;
+                var c = Left.Right.Right;
+                var d = Right;
+
+                return new TreeNode<T>(a, b, c, d, Left.Value, Value, Left.Right.Value);
+            }
+
+            return new TreeNode<T>(this, Value);
+        }
+
+        [Pure]
+        public TreeNode<T> Insert([NotNull] T newValue)
+        {
+            var compareResult = newValue.CompareTo(Value);
+            if (compareResult == 0)
+            {
+                throw new NotSupportedException($"Value {newValue} already exists.");
+            }
+
+            var newLeft = Left;
+            var newRight = Right;
+            if (compareResult < 0)
+            {
+                newLeft = Left?.Insert(newValue) ?? new TreeNode<T>(newValue);
+            }
+
+            if (compareResult > 0)
+            {
+                newRight = Right?.Insert(newValue) ?? new TreeNode<T>(newValue);
+            }
+
+            return new TreeNode<T>(newLeft, newRight, Value).Balance();
+        }
+
+        [Pure]
+        public TreeNode<T> RemoveMin([NotNull] out T minValue)
+        {
+            if (Left == null)
+            {
+                Debug.Assert(Right?.Left == null);
+                Debug.Assert(Right?.Right == null);
+                minValue = Value;
+                return Right;
+            }
+
+            var newLeft = Left.RemoveMin(out minValue);
+            return new TreeNode<T>(newLeft, Right, Value).Balance();
+        }
+
+        [Pure]
+        public TreeNode<T> Remove([NotNull] T valueToRemove, out bool success)
+        {
+            var compareResult = valueToRemove.CompareTo(Value);
+            var value = Value;
+            var newLeft = Left;
+            var newRight = Right;
+
+            if (compareResult < 0)
+            {
+                if (Left == null)
+                {
+                    success = false;
+                    return this;
+                }
+
+                newLeft = Left.Remove(valueToRemove, out success);
+            }
+            else if (compareResult > 0)
+            {
+                if (Right == null)
+                {
+                    success = false;
+                    return this;
+                }
+
+                newRight = Right.Remove(valueToRemove, out success);
+            }
+            else
+            {
+                success = true;
+                if (newRight == null)
+                {
+                    return newLeft;
+                }
+
+                newRight = newRight.RemoveMin(out value);
+            }
+
+            return success ? new TreeNode<T>(newLeft, newRight, value).Balance() : this;
+        }
+
+
+        public TreeNode<T> FindNode([NotNull] T valueToFind)
+        {
+            var compareResult = valueToFind.CompareTo(Value);
+            if (compareResult > 0)
+            {
+                return Right?.FindNode(valueToFind);
+            }
+
+            if (compareResult < 0)
+            {
+                return Left?.FindNode(valueToFind);
+            }
+
+            return this;
+        }
+
+        [Pure]
+        public IEnumerator<T> GetEnumerator()
+        {
+            var stack = new Stack<TreeNode<T>>((int) Math.Pow(2, Height));
+            var visited = new HashSet<TreeNode<T>>();
+            stack.Push(this);
+            while (stack.Any())
+            {
+                var node = stack.Peek();
+                Debug.Assert(node != null, nameof(node) + " != null");
+
+                if (visited.Contains(node))
+                {
+                    yield return node.Value;
+                    stack.Pop();
+                    if (node.Right != null && !visited.Contains(node.Right))
+                    {
+                        stack.Push(node.Right);
+                    }
+
+                    continue;
+                }
+
+                visited.Add(node);
+
+                if (node.Left != null && !visited.Contains(node.Left))
+                {
+                    stack.Push(node.Left);
+                }
+            }
+        }
+
+        [Pure]
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        [Pure]
+        public override string ToString()
+        {
+            return $"Node{{{Value}}}";
+        }
+    }
+
+    internal static class TreeNodeExtensions
+    {
+        public static int SafeHeight<T>(this TreeNode<T> node) where T : IComparable<T>
+        {
+            return node?.Height ?? 0;
+        }
+
+        public static int BalanceFactor<T>([NotNull] this TreeNode<T> node) where T : IComparable<T>
+        {
+            return node.Right.SafeHeight() - node.Left.SafeHeight();
+        }
+    }
+}
