0af1c81a4933495b1a4d8c7def061b6032c7fe36
Anton Zlygostev <zlygostev@hotmail.com>
2020-12-01 09:57:01 +0700

    Adding the R script for generating the benchmark images

---
+++ Atropos.Benchmarks/RibbonGraph.R
@@ -0,0 +1,48 @@
+list.of.packages <- c("ggplot2", "dplyr", "gdata", "tidyr", "grid", "gridExtra", "stringr", "Rcpp", "R.devices")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, lib = Sys.getenv("R_LIBS_USER"), repos = "https://cran.rstudio.com/")
+
+library(ggplot2)
+library(dplyr)
+library(gdata)
+library(tidyr)
+library(grid)
+library(gridExtra)
+library(R.devices)
+library(stringr)
+
+ends_with <- function(vars, match, ignore.case = TRUE) {
+  if (ignore.case)
+    match <- tolower(match)
+  n <- nchar(match)
+
+  if (ignore.case)
+    vars <- tolower(vars)
+  length <- nchar(vars)
+
+  substr(vars, pmax(1, length - n + 1), length) == match
+}
+
+ggsaveNice <- function(fileName, p, ...) {
+  suppressGraphics(ggsave(fileName, plot = p, ...))
+}
+
+args <- commandArgs(trailingOnly = TRUE)
+resPath <- "./bin/release/netcoreapp3.1/BenchmarkDotNet.Artifacts/results"
+
+files <- if (length(args) > 0) args else list.files(resPath)[list.files(resPath) %>% ends_with("-report.csv")]
+
+for (file in files) {
+  title <- gsub("-report.csv", "", basename(file))
+
+  report <- read.csv(paste(resPath, file, sep="/"), sep=",")
+  report$Mean <- as.numeric(gsub(",","", str_sub(report$Mean, end=-5)))
+  report$Error <- as.numeric(gsub(",","", str_sub(report$Error, end=-5)))
+
+  plot <- ggplot(report, aes(x=Size, y=Mean, ymin=Mean-Error, ymax=Mean+Error, color=Method)) +
+    geom_ribbon(aes(fill=Method, alpha=0.5), linetype=0, show.legend=FALSE) +
+    geom_line(size=1) +
+    geom_point(size=1.5) +
+    scale_x_continuous(trans="log2") 
+  ggsaveNice(paste(title, ".png", sep=""), plot, width=13, height=8, dpi=150)
+}
