3ab2e3f74f42f869419bc88941456876b7225e69
Anton Zlygostev <zlygostev@hotmail.com>
2020-12-05 19:37:40 +0700

    Running more Deque benchmarks

--- Atropos.Benchmarks/Deque/DequeBase.cs
+++ Atropos.Benchmarks/Deque/DequeBase.cs
@@ -7,6 +7,10 @@ namespace Atropos.Benchmarks.Deque
 {
     public abstract class DequeBase<T, R> : BenchmarkBase<IImmutableQueue<T>, ImmutableDeque<T>, ImmutableQueue<T>, ImmutableTreeQueue<T>, R>
     {
-        public static IEnumerable<int> DequeSizes() => Enumerable.Range(1, 32);
+        public static IEnumerable<int> DequeSizes()
+        {
+            foreach (var i in Enumerable.Range(0, 16))
+                yield return (1 << i);
+        }
     }
 }
